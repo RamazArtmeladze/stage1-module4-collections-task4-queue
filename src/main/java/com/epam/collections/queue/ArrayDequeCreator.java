e5ba570f4744abfb9db2784d07d635924645606f
Ramaz Artmeladze <ramaz.artmeladze@outlook.com>
2023-10-30 00:35:37 +0400

    Update

--- src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
+++ src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
@@ -1,34 +1,45 @@
 package com.epam.collections.queue;
 
 import java.util.ArrayDeque;
-import java.util.List;
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.Queue;
 
 public class ArrayDequeCreator {
     public ArrayDeque<Integer> createArrayDeque(Queue<Integer> firstQueue, Queue<Integer> secondQueue) {
-        ArrayDeque<Integer> combinedDeque = new ArrayDeque<>();
+        ArrayDeque<Integer> arrayDeque = new ArrayDeque<>();
 
         while (!firstQueue.isEmpty() && !secondQueue.isEmpty()) {
-            // Player 1's turn
-            combinedDeque.add(firstQueue.poll()); // Take the top card from combinedDeque and add to firstQueue
-            combinedDeque.add(secondQueue.poll()); // Take two cards from secondQueue and add to combinedDeque
+            // Add two cards to the ArrayDeque
+            arrayDeque.add(firstQueue.poll());
+            arrayDeque.add(secondQueue.poll());
 
-            // Player 2's turn
-            combinedDeque.add(secondQueue.poll()); // Take the top card from combinedDeque and add to secondQueue
-            combinedDeque.add(firstQueue.poll()); // Take two cards from firstQueue and add to combinedDeque
+            // Take the top card from ArrayDeque and add it to the end of the player's Queue
+            firstQueue.offer(arrayDeque.poll());
+
+            // Put two cards from the beginning of the player's Queue into the ArrayDeque
+            arrayDeque.add(firstQueue.poll());
+            arrayDeque.add(firstQueue.poll());
+
+            // Repeat the process for the second player
+            secondQueue.offer(arrayDeque.poll());
+            arrayDeque.add(secondQueue.poll());
+            arrayDeque.add(secondQueue.poll());
         }
 
-        return combinedDeque;
+        return arrayDeque;
     }
 
     public static void main(String[] args) {
-        ArrayDequeCreator creator = new ArrayDequeCreator();
-        Queue<Integer> firstQueue = new ArrayDeque<>(List.of(1, 5, 7, 6));
-        Queue<Integer> secondQueue = new ArrayDeque<>(List.of(2, 5, 8, 3));
+        Queue<Integer> firstQueue = new LinkedList<>(Arrays.asList(1, 5, 7, 6));
+        Queue<Integer> secondQueue = new LinkedList<>(Arrays.asList(2, 5, 8, 3));
 
+        ArrayDequeCreator creator = new ArrayDequeCreator();
         ArrayDeque<Integer> result = creator.createArrayDeque(firstQueue, secondQueue);
+
         System.out.println(result);
     }
 }
+
 
 
