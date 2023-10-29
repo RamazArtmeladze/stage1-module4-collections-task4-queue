5c2493b5f4086a582351eda6ec43af45013e2aa1
Ramaz Artmeladze <ramaz.artmeladze@outlook.com>
2023-10-29 18:14:03 +0400

    Update

--- src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
+++ src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
@@ -1,10 +1,27 @@
 package com.epam.collections.queue;
 
-import java.util.ArrayDeque;
-import java.util.PriorityQueue;
-import java.util.Queue;
+import java.util.*;
 
-public class ArrayDequeCreator extends PriorityQueue<String> {
+public class ArrayDequeCreator {
     public ArrayDeque<Integer> createArrayDeque(Queue<Integer> firstQueue, Queue<Integer> secondQueue) {
+        ArrayDeque<Integer> arrayDeque = new ArrayDeque<>();
+
+        while (!firstQueue.isEmpty() && !secondQueue.isEmpty()) {
+            arrayDeque.add(firstQueue.poll());
+            arrayDeque.add(secondQueue.poll());
+            firstQueue.add(arrayDeque.poll());
+            firstQueue.add(secondQueue.poll());
+        }
+
+        while (!firstQueue.isEmpty()) {
+            arrayDeque.add(firstQueue.poll());
+        }
+
+        while (!secondQueue.isEmpty()) {
+            arrayDeque.add(secondQueue.poll());
+        }
+
+        return arrayDeque;
     }
 }
+
--- src/main/java/com/epam/collections/queue/PriorityQueueCreator.java
+++ src/main/java/com/epam/collections/queue/PriorityQueueCreator.java
@@ -1,9 +1,16 @@
 package com.epam.collections.queue;
 
-import java.util.List;
-import java.util.PriorityQueue;
+import java.util.*;
 
 public class PriorityQueueCreator {
     public PriorityQueue<String> createPriorityQueue(List<String> firstList, List<String> secondList) {
+        List<String> mergedList = new ArrayList<>(firstList);
+        mergedList.addAll(secondList);
+
+        PriorityQueue<String> priorityQueue = new PriorityQueue<>(Comparator.reverseOrder());
+        priorityQueue.addAll(mergedList);
+
+        return priorityQueue;
     }
 }
+
